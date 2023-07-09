7391d3aa1030c5709cadf99858de138835f6530b
kinco2030 <112095553+kinco2030@users.noreply.github.com>
2023-07-09 16:04:09 +0900

    완성

--- Heungbu-Nolbu-Jeon/Assets/Scripts/Main.cs
+++ Heungbu-Nolbu-Jeon/Assets/Scripts/Main.cs
@@ -10,6 +10,16 @@ public class Main : MonoBehaviour
         SceneManager.LoadScene("StoryScene");
     }
 
+    public void OnClickDescriptionButton()
+    {
+        SceneManager.LoadScene("DescriptionScene");
+    }
+
+    public void OnClickGoBackMainButton()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
     public void OnClickExitButton()
     {
         Application.Quit();
